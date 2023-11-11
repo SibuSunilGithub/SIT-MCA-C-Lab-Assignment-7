96cd256d2876f195c6a2eb6af0826287e170da78
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-11-12 00:33:35 +0530

    Pattern Printing ...
    Star Pattern
    
    *
    * *
    * * *
    * * * *
    * * * * *
    
    Number Pattern
    
    1
    1 2
    1 2 3
    1 2 3 4
    1 2 3 4 5

--- q01_star.c
+++ q01_star.c
@@ -1,4 +1,5 @@
 /*
+Star Pattern
 
 *
 * *
@@ -6,11 +7,20 @@
 * * * *
 * * * * *
 
+Number Pattern
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
 */
 
 #include <stdio.h>
 int main()
 {
+    printf("******* Star Pattern *******\n");
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j <= i; j++)
@@ -19,4 +29,17 @@ int main()
         }
         printf("\n");
     }
+
+    // For Number Pattern
+    printf("***** Number Pattern ******\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%d ", j + 1);
+        }
+        printf("\n");
+    }
+    return 0;
 }
